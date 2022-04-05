d03c0621c7b2cf06d8c421d261c0c05d0fd1b69e
José Sandro Rogério Mendes de Araújo (UFPE) <jsrma@dmz.org.br>
2022-04-05 14:35:59 -0300

    Projeto cliente-ms criado

---
+++ cliente-ms/src/test/java/com/boaentrega/clientems/ClienteMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boaentrega.clientems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClienteMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
