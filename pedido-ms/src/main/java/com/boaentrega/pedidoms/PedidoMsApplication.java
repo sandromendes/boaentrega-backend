6c2d84cf8ea2ad05079c086272b3107d57910980
José Sandro Rogério Mendes de Araújo (UFPE) <jsrma@dmz.org.br>
2022-04-05 21:18:55 -0300

    Project pedido-ms criado

---
+++ pedido-ms/src/main/java/com/boaentrega/pedidoms/PedidoMsApplication.java
@@ -0,0 +1,13 @@
+package com.boaentrega.pedidoms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidoMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidoMsApplication.class, args);
+	}
+
+}
---
+++ pedido-ms/src/test/java/com/boaentrega/pedidoms/PedidoMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boaentrega.pedidoms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidoMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
