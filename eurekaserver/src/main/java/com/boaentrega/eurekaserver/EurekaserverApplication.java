ee7783cee6872c3d3429a899604bb5208ceabd61
José Sandro Rogério Mendes de Araújo (UFPE) <jsrma@dmz.org.br>
2022-04-07 08:56:26 -0300

    Projeto Eureka Server criado

---
+++ eurekaserver/src/main/java/com/boaentrega/eurekaserver/EurekaserverApplication.java
@@ -0,0 +1,13 @@
+package com.boaentrega.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverApplication.class, args);
+	}
+
+}
