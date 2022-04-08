021b677b52d0227a39bd28e9a72e9cabed914e2b
José Sandro Rogério Mendes de Araújo (UFPE) <jsrma@dmz.org.br>
2022-04-08 15:27:58 -0300

    Actuator config-server

--- cliente-ms/src/main/java/com/boaentrega/clientems/resources/ClienteResource.java
+++ cliente-ms/src/main/java/com/boaentrega/clientems/resources/ClienteResource.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -22,6 +23,7 @@ import com.boaentrega.clientems.dto.ClienteDTO;
 import com.boaentrega.clientems.service.ClienteService;
 import com.boaentrega.clientems.utils.Mapper;
 
+@RefreshScope
 @RestController
 @RequestMapping(value = "/clientes")
 public class ClienteResource {
