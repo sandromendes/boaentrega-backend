ba9b09f40d570a7640f696e89270acb2cb83b542
José Sandro Rogério Mendes de Araújo (UFPE) <jsrma@dmz.org.br>
2022-04-10 10:25:03 -0300

    Criação do oauth-ms

---
+++ oauth-ms/src/main/java/com/boaentrega/oauthms/OauthMsApplication.java
@@ -0,0 +1,13 @@
+package com.boaentrega.oauthms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthMsApplication.class, args);
+	}
+
+}
